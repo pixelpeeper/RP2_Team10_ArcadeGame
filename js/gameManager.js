16a169813e715ba9ac2cd2cfb1a8558acf10a22c
u1328124@utah.edu <u1328124@utah.edu>
2021-09-14 11:22:55 -0600

    Fire in y axis

--- js/gameManager.js
+++ js/gameManager.js
@@ -23,28 +23,31 @@ var firebutton;
 
 function preload ()
 {
-    this.load.image('bullet', 'images/Bullet.png');
+  this.load.image('bullet', 'images/Bullet.png');
 	this.load.image('ship', 'images/Spaceship1.png');
 }
 
 function create ()
 {
 	bullets = this.add.group();
-    bullets.enableBody = true;
-    bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    bullets.createMultiple(30, 'bullet');
+  bullets.enableBody = true;
+  bullets.physicsBodyType = Phaser.Physics.ARCADE;
+  bullets.createMultiple(30, 'bullet');
+
+
 //    bullets.setAll('anchor.x', 0.5);
   //  bullets.setAll('anchor.y', 1);
  //   bullets.setAll('outOfBoundsKill', true);
  //   bullets.setAll('checkWorldBounds', true);
+  fireBullet.bind(this);
 	player = this.physics.add.sprite(400, 400, 'ship');
 	player.displayWidth=50;
 	player.scaleY = player.scaleX;
-    player.setAngle(-90);
-    player.setDamping(true);
-    player.setDrag(0.99);
-    player.setMaxVelocity(200);
-    cursors = this.input.keyboard.createCursorKeys();
+  player.setAngle(-90);
+  player.setDamping(true);
+  player.setDrag(0.99);
+  player.setMaxVelocity(200);
+  cursors = this.input.keyboard.createCursorKeys();
 	//firebutton = this.input.keyboard.addKey(Phaser.keyboard.SPACEBAR);
 }
 function update ()
@@ -58,7 +61,7 @@ function update ()
         {
             player.setAcceleration(0);
         }
-        
+
         // check for rotation
         if (cursors.left.isDown)
         {
@@ -72,25 +75,32 @@ function update ()
         {
             player.setAngularVelocity(0);
         }
-		if (cursors.down.isDown) 
-		{
-        fireBullet(bullets);    
-		}else{};
-	
-        
+		    if (cursors.down.isDown)
+		    {
+          //  var newbullet = this.physics.add.sprite(player.x, player.y + 5, 'bullet');
+          //  newbullet.body.velocity.y = -400;
+        var BULLET_SPEED = 300;
+        var bulletOffset = 20 * Math.sin(player.angle * 3.14 / 180 );
+        var newbullet = this.physics.add.sprite(player.x + bulletOffset, player.y);
+        newbullet.angle = player.angle;
+        this.physics.Arcade.ArcadePhysics.velocityFromAngle(newbullet.angle - 90, BULLET_SPEED, newbullet.body.velocity);
+        newbullet.body.velocity.x += player.body.velocity.x;
+      }else{};
+
+
         this.physics.world.wrap(player, 32);
 }
 
-function fireBullet(bullets)
+function fireBullet()
 {
-	var bullet = bullets.getFirstExists(false);
+	//var bullet = bullets.getFirstExists(false);
+ this.physics.add.sprite(player.x, player.y + 5, 'bullet');
 
-    if (bullet)
-    {
+  //  if (bullet)
+  //  {
         //  And fire it
-		bullet.create(player.x, player.y + 8, 'bullet');
-        bullet.reset(player.x, player.y + 8);
-        bullet.body.velocity.y = -400;
-    }
+		//bullet.create(player.x, player.y + 8, 'bullet');
+    //    bullet.reset(player.x, player.y + 8);
+    //    bullet.body.velocity.y = -400;
+  //  }
 }
-
