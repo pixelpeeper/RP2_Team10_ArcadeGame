61c9d8c7d87aff55ef45675cec1de2cb5154e8d2
Avery <pixelpeeper@users.noreply.github.com>
2021-09-16 14:30:59 -0600

    added game dimming when pause menu is displayed

--- js/pause.js
+++ js/pause.js
@@ -17,6 +17,10 @@ class pauseScreen extends Phaser.Scene {
 	}
 	
 	create(){
+		var pauseBackground = this.add.rectangle(0,0, gameDimentions.x, gameDimentions.y, 0x333333);
+		pauseBackground.alpha = 0.5;
+		pauseBackground.setOrigin(0,0);
+		
 		console.log('pause creating');
 		gameDimentions.x = this.sys.game.canvas.width;
 		gameDimentions.y = this.sys.game.canvas.height;
