4bc00a173911717d503b719df1a6a8f93004f22a
Avery <pixelpeeper@users.noreply.github.com>
2021-09-18 12:41:09 -0600

    moved tweenHelper class to index.js

--- js/fail.js
+++ js/fail.js
@@ -43,46 +43,4 @@ class failScreen extends Phaser.Scene {
 	}
 }
 
-
-class TweenHelper {
-    static flashElement(scene, element, repeat = true, easing = 'sine.inout', overallDuration = 750, visiblePauseDuration = 250) {
-        if (scene && element) {
-            let flashDuration = overallDuration - visiblePauseDuration / 2;
-
-            scene.tweens.timeline({
-                tweens: [
-                    {
-                        targets: element,
-                        duration: 0,
-                        alpha: 0,
-                        ease: easing
-                    },
-                    {
-                        targets: element,
-                        duration: flashDuration,
-                        alpha: 1,
-                        ease: easing
-                    },
-                    {
-                        targets: element,
-                        duration: visiblePauseDuration,
-                        alpha: 1,
-                        ease: easing
-                    },
-                    {
-                        targets: element,
-                        duration: flashDuration,
-                        alpha: 0,
-                        ease: easing,
-                        onComplete: () => {
-                            if (repeat === true) {
-                                this.flashElement(scene, element);
-                            }
-                        }
-                    }
-                ]
-            });
-        }
-    }
-}
 //put functions here
