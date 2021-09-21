34fdbe4a7e8d60f5250449b821cea29920d63a45
Jonathan Conger <44207939+jonathanconger@users.noreply.github.com>
2021-09-20 19:31:55 -0600

    added game audio

--- js/gameManager.js
+++ js/gameManager.js
@@ -18,15 +18,17 @@ class gameScreen extends Phaser.Scene{
 		this.load.svg('asteroidsmall', 'images/asteroid3.svg');
 
 		// audio assets
-		this.load.audio('bgm', 'audio/.mp3');
+		this.load.audio('bgm', 'audio/BG_Music.mp3');
 		// player sounds
-		this.load.audio('gun1', 'audio/.mp3');
-		this.load.audio('gun2', 'audio/.mp3');
-		this.load.audio('thrust1', 'audio/.mp3');
-		this.load.audio('thrust2', 'audio/.mp3');
-		this.load.audio('explosion', 'audio/.mp3');
+		this.load.audio('gun1', 'audio/Push_Gun_1.mp3');
+		this.load.audio('gun2', 'audio/Push_Gun_2.mp3');
+		this.load.audio('thrust1', 'audio/1_Thrust.mp3');
+		this.load.audio('thrust2', 'audio/Thrust_2.mp3');
+		//this.load.audio('explosion', 'audio/.mp3');
 		// asteroid sounds
-		this.load.audio('coll1', 'audio/.mp3');
+		this.load.audio('coll1', 'audio/Collision_1.mp3');
+		this.load.audio('coll2', 'audio/Collision_2.mp3');
+		this.load.audio('coll3', 'audio/Collision_3.mp3');
 
 		//play like this
 		// var music = this.sound.add('filename');
@@ -41,7 +43,7 @@ class gameScreen extends Phaser.Scene{
 		//game parameters
 		this.score = 0;
 		this.scoreIncrement = 50;
-		this.asteroidIncrease = 3;
+		this.asteroidIncrease = 2;
 		this.level = 1;
 
 		this.blasts = this.physics.add.group();
@@ -62,6 +64,7 @@ class gameScreen extends Phaser.Scene{
 		globalTHIS = this;
 
 		this.asteroidController = new AsteroidController();
+		this.soundController = new SoundController(this);
 		this.hud = new HUD(this);
 		this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -79,6 +82,8 @@ class gameScreen extends Phaser.Scene{
 			this.scene.launch('pauseScreen');
 			this.scene.pause('gameScreen');
 		});
+
+		this.soundController.playBGM();
 	}
 
 	update ()
@@ -104,7 +109,7 @@ class gameScreen extends Phaser.Scene{
 				this.level += 1;
 				this.hud.updateLevel(this.level);
 				finalcount = this.level * this.asteroidIncrease;
-				spawnAsteroidWave(this, this.level * this.asteroidIncrease); //make the new asteroids a function of the level & whatever difficulty multiplier we want
+				spawnAsteroidWave(this, this.level + this.asteroidIncrease); //make the new asteroids a function of the level & whatever difficulty multiplier we want
 			}
 		} else {
 			//console.log('player is dead');
@@ -117,6 +122,7 @@ class gameScreen extends Phaser.Scene{
 
 function createColliders(scene) {
 	scene.physics.add.collider(scene.players, scene.asteroids, function (player, asteroid){
+		scene.soundController.playCollision();
 		scene.player.killPlayer(player, asteroid, scene);
 	});
 	scene.physics.add.collider(scene.asteroids, scene.asteroids, (asteroid1, asteroid2) =>{
@@ -131,29 +137,33 @@ function createColliders(scene) {
 			scene.hud.updateScore(scene.score);
 			}
 		}
+
+		scene.soundController.playCollision();
 	}); //asteroid self-collisions
 
 	scene.physics.add.collider(scene.blasts, scene.asteroids, (blast, asteroid) => {
 		if(finalcount==1 && asteroid.type != 0 )
 		{
 			asteroid.destroyAsteroid();
-		}
-		
+		}		
 		else
 		{
-		if(asteroid.type == 0)
-		{ 
-			asteroid.destroyAsteroid();
-			scene.score += scene.scoreIncrement;
-			scene.hud.updateScore(scene.score);
+			if(asteroid.type == 0)
+			{ 
+				asteroid.destroyAsteroid();
+				scene.score += scene.scoreIncrement;
+				scene.hud.updateScore(scene.score);
+			}
+			else if(asteroid.getactivated() == false && asteroid.type!=0)
+			{
+				asteroid.setactivated(true);
+				this.time = scene.time.addEvent({delay: 4000, callback: () => {asteroid.setactivated(false)},
+				scope: this})
+			}
 		}
-		else if(asteroid.getactivated() == false && asteroid.type!=0)
-		{
-			asteroid.setactivated(true);
-			this.time = scene.time.addEvent({delay: 4000, callback: () => {asteroid.setactivated(false)},
-			scope: this})
-		}
-		}
+
+		scene.soundController.playCollision();
+
 		//update score here
 		delete blast.destroy();
 		
@@ -245,6 +255,8 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 				this.scene.player.rotation,
 				200,
 				this.scene.player.body.acceleration);
+			//play thrust sound	
+			this.scene.soundController.playThrust();
 		}
 		else
 		{
@@ -270,6 +282,9 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 		if (this.scene.cursors.space.isDown && this.bulletTime <= this.scene.time.now && this.scene.blasts.getLength() < 3) {
 			this.bulletTime = this.scene.time.now + this.bulletFrequency;
 			new Blast(this.scene);
+
+			//play blast sound
+			this.scene.soundController.playGun();
 		}
 	}
 
@@ -316,36 +331,8 @@ class Blast extends Phaser.Physics.Arcade.Sprite {
 		this.setOffset(0.5, 0.5);
 	}
 }
-// Below function will create small asteroids on collision
-function createnewsmallasteroids(_asteroid1,_asteroid2)
-{
-	createsmallasteroids(
-		Phaser.Math.Average([_asteroid1.body.center.x, _asteroid2.body.center.x]) + Phaser.Math.Between(-32,32),
-		Phaser.Math.Average([_asteroid1.body.center.y, _asteroid2.body.center.y]) + Phaser.Math.Between(-32,32),
-		Phaser.Math.Average([_asteroid1.body.velocity.x, _asteroid2.body.velocity.x]) * Phaser.Math.Between(-4,4),
-		Phaser.Math.Average([_asteroid1.body.velocity.y, _asteroid2.body.velocity.y]) * Phaser.Math.Between(-4,4),
-
-	);
-	_asteroid1.disableBody(true,true);
-	_asteroid2.disableBody(true,true);
-}
-// Below function will create medium asteroids on collision
-function createnewmediumasteroids(_asteroid1,_asteroid2)
-{
-	createmediumasteroids(
-		Phaser.Math.Average([_asteroid1.body.center.x, _asteroid2.body.center.x]) + Phaser.Math.Between(-32,32),
-		Phaser.Math.Average([_asteroid1.body.center.y, _asteroid2.body.center.y]) + Phaser.Math.Between(-32,32),
-		Phaser.Math.Average([_asteroid1.body.velocity.x, _asteroid2.body.velocity.x]) * Phaser.Math.Between(-4,4),
-		Phaser.Math.Average([_asteroid1.body.velocity.y, _asteroid2.body.velocity.y]) * Phaser.Math.Between(-4,4),
-
-	);
-	_asteroid1.disableBody(true,true);
-	_asteroid2.disableBody(true,true);
-}
-
 
 class AsteroidController {
-
 		 genAsteroids(scene, num) {
 			 for (let i = 0; i < num; i++) {
 				 this.spawnAsteroid(scene);
@@ -620,5 +607,67 @@ class HUD {
 	displayGameOverOverlay() {
 		this.gameOverOverlay.gameOverText.visible = true;
 		this.gameOverOverlay.restartText.visible = true;
+	}
+}
+
+class SoundController {
+	constructor(scene) {
+		this.scene = scene;
+		
+		// initialize sounds
+		scene.bgm = scene.sound.add('bgm', { loop: true });
+		scene.gun1 = scene.sound.add('gun1');
+		scene.gun2 = scene.sound.add('gun2');
+		scene.thrust1 = scene.sound.add('thrust1');
+		scene.thrust2 = scene.sound.add('thrust2');
+		scene.coll1 = scene.sound.add('coll1');
+		scene.coll2 = scene.sound.add('coll2');
+		scene.coll3 = scene.sound.add('coll3');
+	}
+
+	playBGM() {
+		this.scene.bgm.play();
+	}
+
+	playGun() {
+		switch (Math.floor(Math.random() * 2)) {
+			case 0:
+				this.scene.gun1.play();
+				break;
+			case 1:
+				this.scene.gun2.play();
+				break;
+			default:
+				break;	
+		}
+	}
+
+	playThrust() {
+		switch (Math.floor(Math.random() * 2)) {
+			case 0:
+				this.scene.thrust1.play();
+				break;
+			case 1:
+				this.scene.thrust2.play();
+				break;
+			default:
+				break;	
+		}
+	}
+
+	playCollision() {
+		switch (Math.floor(Math.random() * 3)) {
+			case 0:
+				this.scene.coll1.play();
+				break;
+			case 1:
+				this.scene.coll2.play();
+				break;
+			case 2:
+				this.scene.coll3.play();
+				break;
+			default:
+				break;	
+		}
 	}
 }
